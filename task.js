3773cfe0c15f76e0f29930d8338ee8c4a4c8b111
kucheryaska <kucheryaska12@gmail.com>
2026-04-23 13:22:59 +0300

    JS_5.3_v1

---
+++ task.js
@@ -0,0 +1,21 @@
+let xhr = new XMLHttpRequest();
+const form = document.getElementById('form');
+const progress = document.getElementById( 'progress' );
+
+
+const changeProgress = function(e) {
+		progress.value = e.loaded / e.total
+}
+
+xhr.upload.addEventListener('progress', (e)=>{
+    console.log('событие');
+	changeProgress(e);
+	});
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let formData = new FormData(form);
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.send(formData);
+})
